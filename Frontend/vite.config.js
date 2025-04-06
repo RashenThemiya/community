4c409a963c9abce7cb42f4ace029f0d5f7cb3a86
rashen <rashenrashen4@gmail.com>
2025-04-06 22:55:45 +0530

    fix frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,23 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Split vendor libraries into a separate chunk
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // This will create a 'vendor' chunk for all node_modules libraries
+            return 'vendor';
+          }
+        },
+      },
+    },
+    // Increase chunk size limit if necessary
+    chunkSizeWarningLimit: 1000, // 1MB for example (default is 500KB)
+  },
 })
