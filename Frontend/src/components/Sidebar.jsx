221c722bbf72a172d85c9dc84f1a28a82ab5ac91
rashen <rashenrashen4@gmail.com>
2025-04-15 21:32:16 +0530

    Update Sidebar.jsx

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -7,6 +7,9 @@ import {
     FaFileInvoice,
     FaHome,
     FaMoneyBill,
+    FaNewspaper // Import the FaNewspaper icon for Publication
+    ,
+
     FaSignOutAlt,
     FaStore,
     FaTag,
@@ -25,11 +28,12 @@ const menuItems = [
     { path: "/payments", icon: <FaMoneyBill />, label: "Payments" },
     { path: "/invoices", icon: <FaFileInvoice />, label: "Invoices" },
     { path: "/reports", icon: <FaChartBar />, label: "Reports" },
-    { path: "/product-management", icon:  <FaBox />, label: "Product" },
+    { path: "/product-management", icon: <FaBox />, label: "Product" },
     { path: "/daily-price", icon: <FaTag />, label: "Daily Price" },
     { path: "/vehicle-ticketing", icon: <FaTruckPickup />, label: "Vehicle Ticketing" },
     { path: "/sanitation-ticketing", icon: <FaToilet />, label: "Sanitation Ticketing" },
     { path: "/settings", icon: <FaCog />, label: "Settings" },
+    { path: "/publication", icon: <FaNewspaper />, label: "Publication" }, // Added Publication menu item
 ];
 
 const Sidebar = () => {
