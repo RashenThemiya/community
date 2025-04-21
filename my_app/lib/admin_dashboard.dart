e1e617fda25779512112fd19566240de2b8c4094
rashen <rashenrashen4@gmail.com>
2025-04-21 17:36:38 +0530

    Update admin dashboard UI and functionality

--- my_app/lib/admin_dashboard.dart
+++ my_app/lib/admin_dashboard.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
-class AdminDashboard extends StatelessWidget {
+import '../components/custom_navbar.dart';
+import '../screens/sanitation_ticketing.dart';
+import '../screens/vehicle_ticketing.dart';
+
+class AdminDashboard extends StatefulWidget {
+  @override
+  _AdminDashboardState createState() => _AdminDashboardState();
+}
+
+class _AdminDashboardState extends State<AdminDashboard> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    VehicleTicketingPage(),
+    SanitationTicketingPage(),
+    Container(), // Placeholder for logout
+  ];
+
+  void _onItemTapped(int index) {
+    if (index == 2) {
+      // Handle logout
+      Navigator.pushReplacementNamed(context, '/login');
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Admin Dashboard')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to Admin Dashboard!',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text('Logout'),
-            ),
-          ],
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CustomNavBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
