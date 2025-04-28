d52f8f233d16226b51171ebc031e0a48de524434
rashen <rashenrashen4@gmail.com>
2025-04-28 10:35:35 +0530

    report

--- rent-collection-backend/test/generateInvoiceTest.js
+++ rent-collection-backend/test/generateInvoiceTest.js
@@ -15,7 +15,7 @@ async function generateInvoicesForAllShops() {
 
         for (const shop of shops) {
             const shop_id = shop.shop_id;
-            const month_year = "2025-06"; // Change to the desired month
+            const month_year = "2025-04"; // Change to the desired month
 
             try {
                 console.log(`🔄 Generating Invoice for Shop: ${shop_id}, Month: ${month_year}`);
