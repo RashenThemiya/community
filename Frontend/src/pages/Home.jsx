d8abddeee40b3d71fafc664c7d2093377a09e213
udara <kumuduelkaduwa@gmail.com>
2025-05-04 13:54:21 +0530

    welcome message updated

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,7 +7,6 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import EconomicCenterNews from "../components/EconomicCenterNews";
 
-
 // Import slick-carousel styles
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -59,8 +58,12 @@ const Home = () => {
             {/* Welcome Section */}
             <div className="container mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
                 <div className="text-left space-y-6">
-                    <h1 className="text-3xl font-bold">{t("home.welcomeMessage")}</h1>
-                    <p className="text-lg text-gray-700 text-justify">{t("home.description")}</p>
+                    <h1 className="text-4xl font-bold text-blue-800">
+                        {t("home.welcomeMessage")}
+                    </h1>
+                    <p className="text-lg text-gray-700 text-justify">
+                        {t("home.description")}
+                    </p>
                 </div>
 
                 <div>
